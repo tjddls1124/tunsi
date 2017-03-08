aefce0ea481775d6d9bb34fdbcd6c819b669a1be
tjddls1124 <tjddls1124@nate.com>
2017-03-08 23:05:48 +0900

    Hello World 과제완료!

--- app/src/main/java/com/example/sungin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sungin/myapplication/MainActivity.java
@@ -9,7 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello world! <Sung-In> ");
 
     }
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!  <Sung-In>");
+
+    }
+}
 }
